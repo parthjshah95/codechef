f0b385a06e796c073bb24bb968951b835d2fc15e
xorcist-part <parthjshah95@gmail.com>
2016-07-20 12:37:58 +0530

    inputs template

---
+++ Problems/Inputs.scala
@@ -0,0 +1,14 @@
+package Problems
+
+/**
+  * Created by parshah on 20-Jul-16.
+  */
+/*
+This
+ */
+object Inputs {
+def inputs: Unit ={
+  val T = io.StdIn.readInt()
+  val inp : List [Int]  = io.StdIn.readLine.split(" ").map(_.toInt).toList
+}
+}
