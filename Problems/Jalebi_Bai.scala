ed96ec092a976386745d4c068dea90a4303b8ca8
xorcist-part <parthjshah95@gmail.com>
2016-07-21 13:39:42 +0530

    two new problems- Jalebi (necklace) and rakesh_matrix

---
+++ Problems/Jalebi_Bai.scala
@@ -0,0 +1,10 @@
+package Problems
+
+/**
+  * Created by parshah on 21-Jul-16.
+  */
+object Jalebi_Bai {
+def concat(): List[Int] ={
+
+}
+}
---
+++ Problems/Rakesh_matrix.scala
@@ -0,0 +1,17 @@
+package Problems
+
+import scala.collection.mutable.ListBuffer
+
+/**
+  * Created by parshah on 21-Jul-16.
+  */
+object Rakesh_matrix {
+
+  def main(args: Array[String]): Unit = {
+
+
+  }
+  def temp(): Unit ={
+
+  }
+}
